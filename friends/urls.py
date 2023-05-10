04be71ffe7a89a4f4143cccd5136a94f77bf999b
Dilara <dilara.0880@gmail.com>
2023-05-10 23:50:31 +0500

    add models, views and templates for requesting

--- friends/urls.py
+++ friends/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('register/', views.register_user, name='register_user'),
     path('send_friend_request/', views.send_friend_request, name='send_friend_request'),
+    path('friend_requests/handle/<int:friend_request_id>/<str:action>/', views.handle_request, name='handle_request'),
+
 ]
--- friends/views.py
+++ friends/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import User, FriendRequest
 
@@ -21,5 +21,33 @@ def send_friend_request(request):
                                        receiver_id=receiver_id)
         friend_request.save()
         return JsonResponse({'success': True})
-    return render(request, 'send_request.html')
+    return render(request, 'sent_request.html')
+
+
+def handle_request(request, friend_request_id, action):
+    friend_request = FriendRequest.objects.get(id=friend_request_id)
+
+    if friend_request.receiver == request.user:
+        if action == 'accept':
+            friend_request.status = 'accepted'
+            friend_request.save()
+        elif action == 'reject':
+            friend_request.status = 'rejected'
+            friend_request.save()
+
+    return redirect(friend_request_list)
+
+
+def friend_request_list(request):
+    friend_requests = FriendRequest.objects.filter(receiver=request.user)
+
+    context = {
+        'friend_requests': friend_requests
+    }
+
+    return render(request, 'sent_request.html', context)
+
+
+
+
 
